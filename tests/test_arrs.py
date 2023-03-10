6a0aab0df891359fc19c8d1a3d38607addca9108
plantheday <plantheday6@gmail.com>
2023-03-10 16:57:20 +0300

    Add new tests to test_arrs.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,16 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+
+
+def test_get_negative_number():
+    assert arrs.get([1, 2, 3], -1, "test")
+
+
+def test_slice_zero_len():
+    assert arrs.my_slice([], 1, 3) == []
+
+
+def test_slice_negative_start():
+    assert arrs.my_slice([1, 2, 3, 4], -1, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4], -5, 3) == [1, 2, 3]
